841cfd9ddf19f77d97fa041796ab812042076585
kudakwashe24 <kudakwashe755@gmail.com>
2022-09-03 12:08:29 +0200

    added text field

--- src/views/login.js
+++ src/views/login.js
@@ -1,7 +1,11 @@
+import TextField from '@mui/material/TextField';
+
 export function Login() {
     return (
         <div>
             <h2>This is my login page</h2>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
         </div>
     )
 }

74f04d94cf176d952ac356ca020ba1df285dae22
kudakwashe24 <kudakwashe755@gmail.com>
2022-09-03 13:28:42 +0200

    updates

--- src/views/login.js
+++ src/views/login.js
@@ -1,15 +1,42 @@
 import TextField from '@mui/material/TextField';
 import { Container } from '@mui/system';
+import { useState } from 'react';
+import { Button } from '@mui/material';
 
 export function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
     return (
         <Container>
             <h2>This is my login page</h2>
-            <TextField label="Email" variant="outlined" />
+            <TextField label="Email"
+                variant="outlined"
+                value={email}
+                onChange={(e) => {
+                    console.log(e)
+                    setEmail(e.target.value)
+                }} />
             <br />
             <br />
 
-            <TextField label="Password" variant="filled" />
+            <TextField label="Password"
+                variant="filled"
+                value={password}
+                onChange={(e) => {
+                    console.log(e)
+                    setPassword(e.target.value)
+                }} />
+
+            <br />
+            <br />
+
+            <Button variant="contained" onClick={() => {
+                console.log(email)
+                console.log(password)
+
+            }}>Submit</Button>
         </Container>
     )
 }
